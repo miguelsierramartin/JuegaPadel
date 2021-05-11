5f184ab00d6ee4413a0af6ed00a7200c7f7f4fe5
miguelsierramartin <miguel.sierra.martin@alumnos.upm.es>
2021-05-11 20:19:03 +0200

    Probando los pipelines de CI

--- JuegaPadelWeb/src/Servicios/Catalog/Catalog.Domain/ProductInStock.cs
+++ JuegaPadelWeb/src/Servicios/Catalog/Catalog.Domain/ProductInStock.cs
@@ -6,8 +6,8 @@ namespace Catalog.Domain
 {
     public class ProductInStock
     {
-        public int ProductInStockId { get; set; }
-        public int ProductId { get; set; }
+        public int ProductoEnStockId { get; set; }
+        public int ProductoId { get; set; }
         public int Stock { get; set; }
     }
 }
